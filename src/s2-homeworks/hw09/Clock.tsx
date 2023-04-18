6259681f448ef31e827d2f8266d4c45aebda9682
kravamango <kravamango@gmail.com>
2023-04-18 15:14:00 +0300

    Homework №9 correct 2

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -38,15 +38,15 @@ function Clock() {
         }
     }
 
-    const stringTime = new Date().toLocaleTimeString('ru-Ru') || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringDate = date.toLocaleDateString('ru-Ru') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
+    const stringTime = new Date().toLocaleTimeString('en-US') || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringDate = date.toLocaleDateString('en-US') || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
-    let formDay = new Intl.DateTimeFormat('ru-Ru', {
+    let formDay = new Intl.DateTimeFormat('en-US', {
         weekday: "long",
 
     });
-    let formMonth = new Intl.DateTimeFormat('ru-Ru', {
+    let formMonth = new Intl.DateTimeFormat('en-US', {
         month: "long",
     });
 
